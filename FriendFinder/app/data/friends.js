c75bbe28f4769ae0e1925f9023ab3f39162bf7e5
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-20 19:15:01 -0400

    lots of files

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,14 @@
+const friends = [
+    {
+        name: "Bun",
+        photoURL: "https://via.placeholder.com/150",
+        answers: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Braut",
+        photoURL: "phttps://via.placeholder.com/150",
+        answers: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    }
+];
+
+module.exports.friends = friends;
---
+++ FriendFinder/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const friends = require("./app/data/friends.js");
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("Sever is listening at http://localhost:" + PORT);
+})
