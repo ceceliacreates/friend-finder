e6af4d6f9a85009bd6f26d4bfb4ce4e115d9f170
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-20 23:26:26 -0400

    adds photos for default dogs

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,13 +1,18 @@
 const friends = [
     {
         name: "Bun",
-        photoURL: "https://via.placeholder.com/150",
+        photoURL: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/29178285_10100560626901512_918807404152910992_n.jpg?_nc_cat=107&_nc_ht=scontent.fatl1-2.fna&oh=5ed2719685a90289711b1a06c136f109&oe=5D974BD9",
         answers: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
     },
     {
         name: "Braut",
-        photoURL: "phttps://via.placeholder.com/150",
+        photoURL: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/1929515_506986014322_7598_n.jpg?_nc_cat=108&_nc_ht=scontent.fatl1-2.fna&oh=81aca36aa63d68d1f5c564eb33620f14&oe=5D9D8278",
         answers: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+    {
+        name: "Seth",
+        photoURL: "https://i.imgur.com/JRBf6V0.png",
+        answers: ["5", "4", "2", "5", "5", "3", "1", "3", "2", "2"]
     }
 ];
 
